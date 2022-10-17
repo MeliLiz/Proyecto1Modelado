99a378487f1c8a09cd1d94193bfad26055a97f39
ethand <ethandamian@ciencias.unam.mx>
2022-10-16 19:10:32 -0500

    Cambie un null que causaba fallos

--- Catalogo.java
+++ Catalogo.java
@@ -5,49 +5,49 @@ public class Catalogo {
 
     private ArrayList<Categoria> categoriasCatalogo;
 
-    public Catalogo(){
-        categoriasCatalogo=new ArrayList<Categoria>(null);
+    public Catalogo() {
+        categoriasCatalogo = new ArrayList<Categoria>();
         addCategoria(new Electronica());
         addCategoria(new Electrodomesticos());
         addCategoria(new Alimenticios());
     }
 
-    public ArrayList<Categoria> getCategorias(){
+    public ArrayList<Categoria> getCategorias() {
         return categoriasCatalogo;
     }
 
-    public void setCategoriasDisponibles(ArrayList<Categoria> categorias){
-        categoriasCatalogo=categorias;
+    public void setCategoriasDisponibles(ArrayList<Categoria> categorias) {
+        categoriasCatalogo = categorias;
     }
 
-    public void addCategoria(Categoria categoria){
+    public void addCategoria(Categoria categoria) {
         categoriasCatalogo.add(categoria);
     }
 
-    public void eliminarCategoria(Categoria categoria){
+    public void eliminarCategoria(Categoria categoria) {
         categoriasCatalogo.remove(categoria);
     }
 
-    public void mostrarCatalogos(){
+    public void mostrarCatalogos() {
         for (Categoria categoria : categoriasCatalogo) {
-            System.out.println("\n###"+categoria.getNombre()+"##\n");
-            Iterator iterador =categoria.crearIterador();
+            System.out.println("\n###" + categoria.getNombre() + "##\n");
+            Iterator iterador = categoria.crearIterador();
             while (iterador.hasNext()) {
-                Producto producto=(Producto) iterador.next();
+                Producto producto = (Producto) iterador.next();
                 System.out.println(producto);
             }
         }
     }
 
-    public Producto buscarProducto(int idProducto){
+    public Producto buscarProducto(int idProducto) {
         for (Categoria categoria : categoriasCatalogo) {
-            Iterator iterador=categoria.crearIterador();
-            while(iterador.hasNext()){
-                Producto producto=(Producto)iterador.next();
-                if(producto.getCodigoDeBarras().equals(idProducto)){
+            Iterator iterador = categoria.crearIterador();
+            while (iterador.hasNext()) {
+                Producto producto = (Producto) iterador.next();
+                if (producto.getCodigoDeBarras().equals(idProducto)) {
                     return producto;
                 }
-            }  
+            }
         }
         return null;
     }
