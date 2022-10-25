1ade434353c15be6f8cf77ecf7256427add8604c
ethand <ethandamian@ciencias.unam.mx>
2022-10-24 19:46:00 -0500

    Arreglamos cosas del main

--- Main.java
+++ Main.java
@@ -5,96 +5,112 @@ import java.io.FileInputStream;
 
 public class Main {
 
-    private static Tienda tienda=new Tienda();
-    private static TiendaProxy tiendaProxy=null;;
-    private static Scanner scanner=new Scanner(System.in);
-    private static Scanner scanner2=new Scanner(System.in);
+    private static Tienda tienda = new Tienda();
+    private static TiendaProxy tiendaProxy = null;
+    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner2 = new Scanner(System.in);
 
-    public static void inicio(){
-        principal:
-        do{
-            int opcion=0;
-            while(true){
+    public static void inicio() {
+        principal: do {
+            int opcion = 0;
+            while (true) {
                 System.out.println("Ingresa una opcion");
                 System.out.println("1) Registrarse");
                 System.out.println("2) Iniciar sesión");
                 System.out.println("3) Salir del sistema");
                 try {
-                    opcion=scanner.nextInt();
-                    if(opcion<4&&opcion>0){
+                    System.out.print("Ingrese una opcion: ");
+                    opcion = scanner.nextInt();
+                    scanner.nextLine();
+                    if (opcion < 4 && opcion > 0) {
                         break;
-                    }else{
+                    } else {
                         System.out.println("Opcion no valida");
                     }
                 } catch (InputMismatchException e) {
                     e.printStackTrace();
-                }  
+                }
             }
             switch (opcion) {
                 case 1:
-                    System.out.println("Ingrese su nombre de usuario");
-                    String nombreUsuario=scanner.nextLine();
-                    System.out.println("Ingrese su nombre completo");
-                    String nombreCliente=scanner.nextLine();
-                    System.out.println("Ingrese su contraseña");
-                    String contrasena=scanner.nextLine();
-                    int telefono=0;
-                    while(true){
+                    System.out.print("Ingrese su nombre de usuario: ");
+                    // scanner.nextLine();
+                    String nombreUsuario = scanner.nextLine();
+                    System.out.print("\nIngrese su nombre completo: ");
+                    // scanner.nextLine();
+                    String nombreCliente = scanner.nextLine();
+                    System.out.print("\nIngrese su contraseña: ");
+                    // scanner.nextLine();
+                    String contrasena = scanner.nextLine();
+                    int telefono = 0;
+                    while (true) {
                         try {
-                            System.out.println("Ingrese su número telefónico");
-                            telefono=scanner.nextInt();
+                            System.out.print("\nIngrese su número telefónico (solo 8 numeros): ");
+                            telefono = scanner2.nextInt();
+
                             break;
                         } catch (InputMismatchException e) {
                             System.out.println("Al parecer no ingreso un numero");
                         }
                     }
-                    
-                    System.out.println("Ingrese su dirección");
-                    String direccion=scanner.nextLine();
-                    int numCuenta=0;
-                    while(true){
+
+                    System.out.print("\nIngrese su dirección: ");
+                    // scanner.nextLine();
+                    String direccion = scanner.nextLine();
+                    int numCuenta = 0;
+                    while (true) {
                         try {
-                            System.out.println("Ingrese su número de cuenta bancaria");
-                            numCuenta=scanner.nextInt();
+                            System.out.print("\nIngrese su número de cuenta bancaria: ");
+
+                            numCuenta = scanner2.nextInt();
+                            // scanner.nextLine();
+
                             break;
                         } catch (InputMismatchException e) {
                             System.out.println("Al parecer no ingreso un numero");
                         }
                     }
-                    
-                    System.out.println("Ingrese el país de residencia");
-                    String pais=scanner.nextLine();
-                    Cliente cliente=new Cliente(nombreUsuario, contrasena, nombreCliente, telefono, direccion, numCuenta, pais, tienda.getUltimoIDRegistrado()+1, tienda);
-                    System.out.println("Inicie sesión por favor");
+
+                    System.out.print("\nIngrese el país de residencia: ");
+                    String pais = scanner.nextLine();
+                    Cliente cliente = new Cliente(nombreUsuario, contrasena, nombreCliente, telefono, direccion,
+                            numCuenta, pais, tienda.getUltimoIDRegistrado() + 1, tienda);
+                    System.out.println(tienda.getClientes().size());
+                    System.out.println("\nInicie sesión por favor");
                     break;
                 case 2:
-                    System.out.println("Ingrese su nombre de usuario");
-                    String usuario=scanner2.nextLine();
-                    System.out.println("Ingrese su contraseña");
-                    String key=scanner2.nextLine();
-                    Cliente client =new Cliente(usuario, key);
+                    scanner2.nextLine();
+                    System.out.print("Ingrese su nombre de usuario: ");
+                    String usuario = scanner2.nextLine();
+
+                    System.out.print("Ingrese su contraseña: ");
+                    String key = scanner2.nextLine();
+
+                    Cliente client = new Cliente(usuario, key);
                     tiendaProxy.iniciarSesion(client);
                     break;
                 case 3:
                     break principal;
-            
+
                 default:
                     break;
             }
-        }while(true);
-        
-        
+        } while (true);
+
     }
+
     public static void main(String[] args) {
-        try{
-            ObjectInputStream entrada=new ObjectInputStream(new FileInputStream("archivo.obj"));
-            tienda=(Tienda)entrada.readObject();
+        try {
+            ObjectInputStream entrada = new ObjectInputStream(new FileInputStream("archivo.obj"));
+            tienda = (Tienda) entrada.readObject();
             entrada.close();
-        }catch(Exception e){//Si el archivo no se pudo leer
+        } catch (Exception e) {// Si el archivo no se pudo leer
             System.out.println("No se pudo deserializar");
-            Cliente cliente1=new Cliente("Elec", "elec17", "Elec Kincaid", 96378415, "Rosewood street, Boston", 123456789, "Estados Unidos", 1, tienda);
-        }finally{
-            tiendaProxy=new TiendaProxy(tienda);
+            Cliente cliente1 = new Cliente("Elec", "elec17", "Elec Kincaid", 96378415, "Rosewood street, Boston",
+                    123456789, "Estados Unidos", 1, tienda);
+
+        } finally {
+            tiendaProxy = new TiendaProxy(tienda);
             System.out.println(tienda.getClientes().size());
         }
 
